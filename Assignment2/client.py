95088e133442df487fd9d76e11f3e08d0a27931d
Choseoyoung <seoyoung0000@naver.com>
2022-05-03 18:13:59 +0900

    Edit Assignment2

--- Assignment2/client.py
+++ Assignment2/client.py
@@ -5,23 +5,41 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 while True:
-    flag = input('계속하시겠습니까?(T/F): ')
+    flag = input('로그인 하시겠습니까?(T/F): ')
+    clientSocket.sendto(flag.encode(),(serverName, serverPort))
     if(flag == 'F'):
         break
+    
     #id를 입력받음
     member_id = input('Please enter your id: ')
     clientSocket.sendto(member_id.encode(),(serverName, serverPort))
-    #pw를 입력받음
-    member_pw = input('Please enter your pw: ')
-    clientSocket.sendto(member_pw.encode(),(serverName, serverPort))
-
-    #로그인 결과/회원 등록 결과 출력
+    
+    #id입력 결과 출력/ 비밀번호 확인 or 회원등록 절차
     is_member, serverAddress = clientSocket.recvfrom(2048)
     is_member = is_member.decode()
-    if(is_member == 'Success'):
+    if(is_member=="True"):
+        member_pw = input('Please enter your pw: ')
+    else:
+        #회원등록
+        print(member_id+"님 안녕하세요. 비밀번호를 설정해주세요.")
+        while True:
+            member_pw = input('Please enter your pw: ')
+            tmp = input('Please enter your pw again: ')
+            if(member_pw == tmp):
+                break
+            else:
+                print("비밀번호가 서로 다릅니다. 다시 입력해주세요")
+    clientSocket.sendto(member_pw.encode(),(serverName, serverPort))
+    
+    #회원이면 로그인 성공 여부 출력
+    #비회원이면 회원가입 절차
+    success, serverAddress = clientSocket.recvfrom(2048)
+    success = success.decode()
+    if(success == 'True'):
         print(member_id+"님 로그인되었습니다")
-    elif(is_member == 'Fail'):
+    elif(success == 'True'):
         print("잘못된 id/pw입니다.")
-    elif(is_member == 'False'):
+    elif(success == "New"):
         print("회원등록이 완료되었습니다")
+print("Close the client socket")
 clientSocket.close()
--- Assignment2/server.py
+++ Assignment2/server.py
@@ -7,20 +7,41 @@ serverSocket=socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('',serverPort))
 print("The server is ready to receive")
 while True:
+    #로그인 지속 여부 확인
+    flag, clientAddress = serverSocket.recvfrom(2048)
+    flag = flag.decode()
+    if(flag=='F'):
+        break
+
     member_id, clientAddress = serverSocket.recvfrom(2048)
     member_id = member_id.decode()
-    print(member_id)
+    #회원/비회원 여부 확인
+    if(member_id in ID_table):
+        print(member_id+"님은 회원입니다. 비밀번호가 필요합니다.")
+        is_member = "True"
+    else:
+        print(member_id+"님은 비회원입니다. 회원등록이 필요합니다.")
+        is_member = "False"
+    serverSocket.sendto(is_member.encode(),clientAddress)
+
+    #비밀번호를 입력받음
     member_pw, clientAddress = serverSocket.recvfrom(2048)
     member_pw = member_pw.decode()
-    print(member_pw)
-    #회원이면 PW를 확인함
-    if(member_id in ID_table):
+
+    if(is_member=="True"):
+        #회원이면 비밀번호를 확인함
         if(ID_table[member_id]==member_pw):
-            is_member = 'Success'
+            print("로그인 성공")
+            success = "True"
         else:
-            is_member = 'Fail'
-    #비회원이면 ID_Table에 등록함
+            print("잘못된 id/pw입니다.")
+            success = "False"
     else:
-        is_member = 'False'
+        #비회원이면 ID_Table에 등록함
+        print("신규 회원입니다.")
         ID_table[member_id]=member_pw
-    serverSocket.sendto(is_member.encode(),clientAddress)
+        success = "New"
+
+    serverSocket.sendto(success.encode(),clientAddress)
+print("Close the server socket")
+serverSocket.close()
